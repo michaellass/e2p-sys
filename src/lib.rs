56fbf5ce0cdf44606db294626ca799661d7e02cd
Michael Lass <bevan@bi-co.net>
2019-06-16 23:57:18 +0200

    Lint test routine

--- src/lib.rs
+++ src/lib.rs
@@ -6,38 +6,27 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
     use std::env;
-    use std::fs::{File,remove_file};
-    use std::os::raw::c_char;
     use std::ffi::CString;
+    use std::fs::{remove_file, File};
+    use std::os::raw::c_char;
 
     #[test]
     fn set_and_read_flags() {
         unsafe {
-            let mut path = match env::current_dir(){
-                Ok(p)  => p,
-                Err(_) => panic!("Could not determine current dir"),
-            };
+            let mut path = env::current_dir().expect("Could not determine current dir");
             path.push("e2p-sys_testfile_Gahlu1ka");
-            let path_cstr = match path.to_str() {
-                Some(s) => match CString::new(s) {
-                    Ok(cs) => cs,
-                    Err(_) => panic!("Could not convert str to CStr"),
-                },
-                None    => panic!("Could not convert path to str"),
-            };
-            let path_ptr : *const c_char = path_cstr.as_ptr();
-            let _f = match File::create(&path) {
-                Ok(fh) => fh,
-                Err(_) => panic!("Could not create file"),
-            };
+            let path_cstr = CString::new(path.to_str().expect("Could not convert path to str"))
+                .expect("Could not convert str to CStr");
+            let path_ptr: *const c_char = path_cstr.as_ptr();
+            let _f = File::create(&path).expect("Could not create file");
 
             fsetflags(path_ptr, EXT2_NOATIME_FL as u64);
 
-            let mut readback : u64 = 0;
-            let readback_ptr : *mut u64 = &mut readback;
+            let mut readback: u64 = 0;
+            let readback_ptr: *mut u64 = &mut readback;
             fgetflags(path_ptr, readback_ptr);
 
             drop(_f);
