a599837e77eaa7a64742750c31c75b8bc61395c5
Michael Lass <bevan@bi-co.net>
2019-06-19 23:44:11 +0200

    Add test for parsing an ext2 superblock

--- src/lib.rs
+++ src/lib.rs
@@ -34,8 +34,11 @@ mod tests {
     use super::*;
     use std::env;
     use std::ffi::CString;
-    use std::fs::{remove_file, File};
+    use std::fs::{read_to_string, remove_file, File};
+    use std::io::prelude::*;
     use std::os::raw::c_char;
+    use std::os::unix::prelude::*;
+    use std::mem::transmute;
 
     #[test]
     fn set_and_read_flags() {
@@ -57,6 +60,45 @@ mod tests {
             let _ = remove_file(path);
 
             assert_eq!(readback, EXT2_NOATIME_FL as u64)
+        }
+    }
+
+
+    #[test]
+    fn read_superblock() {
+        unsafe {
+            let mut path = env::current_dir().expect("Could not determine current dir");
+            path.push("test_data");
+            path.push("sb.raw");
+            let mut buf = [0; 1024];
+            let mut f = File::open(path).expect("Could not open superblock");
+            f.read(&mut buf).expect("Could not read from superblock");
+            let mut sb: ext2_super_block = transmute::<[u8; 1024], ext2_super_block>(buf);
+            let sb_ptr: *mut ext2_super_block = &mut sb;
+
+            // Randomly selected values from the superblock
+            assert_eq!(sb.s_inodes_count, 256);
+            assert_eq!(sb.s_blocks_per_group, 8192);
+            assert_eq!(sb.s_magic, 0xEF53);
+
+            path = env::current_dir().expect("Could not determine current dir");
+            path.push("e2p-sys_testfile_eJoo1ca4");
+            f = File::create(&path).expect("Could not create temporary file");
+            let mut mode = 'w' as i8;
+            let mode_ptr: *mut i8 = &mut mode;
+            let fd = fdopen(f.as_raw_fd(), mode_ptr);
+            list_super2(sb_ptr, fd);
+            fclose(fd);
+            drop(f);
+            let fn_out: String = read_to_string(&path).expect("Could not read from output file");
+            let _ = remove_file(path);
+
+            path = env::current_dir().expect("Could not determine current dir");
+            path.push("test_data");
+            path.push("sb.golden");
+            let sb_golden: String = read_to_string(path).expect("Could not read golden output")
+                                    .parse().expect("Could not parse golden output");
+            assert_eq!(fn_out, sb_golden);
         }
     }
 }
