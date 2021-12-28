623545d983c89d860868fc292cb6051236c984ea
Michael Laß <bevan@bi-co.net>
2021-12-28 16:27:15 +0100

    Silence warnings caused by bindgen
    See: https://github.com/rust-lang/rust-bindgen/issues/1651

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,12 @@ SOFTWARE.
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+// https://github.com/rust-lang/rust-bindgen/issues/1651
+// https://github.com/rust-lang/rust-bindgen/issues/2053
+// https://github.com/rust-lang/rust-bindgen/issues/2066
+#![allow(unaligned_references)]
+#![allow(deref_nullptr)]
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 include!(concat!(env!("OUT_DIR"), "/constants.rs"));
 
